18ac6ed758be1030c8153907c0abdbf7c85b6e97
Daichi Baba <11ca058c@gmail.com>
2017-06-23 00:23:35 +0900

    基礎プログラムの作成

---
+++ Assets/script/Base/UIBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBase : MonoBehaviour {
+
+    protected Dictionary<string, object> m_Data;
+    protected Action<object> m_CallBackOnDone;
+    protected string m_UIName;
+
+	public virtual void Init(Dictionary<string, object> _data=null, Action<object> callBackOnDone=null)
+    {
+        m_Data = _data;
+        m_CallBackOnDone = callBackOnDone;
+        m_UIName = this.gameObject.name.Replace("(Clone)", "");
+    }
+
+    public virtual void Hide()
+    {
+        OnHide();
+    }
+    void OnHide()
+    {
+        if (!UIManager.instance.RemoveListPopups(m_UIName))
+        {
+            Debug.LogError("Can't Hide "+ m_UIName);
+            return;
+        }
+        if (m_CallBackOnDone != null)
+            m_CallBackOnDone(m_Data);
+        if (this.gameObject != null)
+            Destroy(this.gameObject);
+    }
+}
---
+++ Assets/script/Manager/UIManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    static protected UIManager m_Instance;
+    static public UIManager instance { get { return m_Instance; } }
+    protected Dictionary<string, UIBase> m_Popups = new Dictionary<string, UIBase>();
+    [SerializeField]
+    protected Transform popup;
+
+    public void ShowPopup(string popupName, Dictionary<string, object> _data, Action<object> callBackOnDone)
+    {
+        if (!m_Popups.ContainsKey(popupName))
+        {
+            GameObject obj = LoadPrefab(popupName);
+            if (obj != null)
+            {
+                obj.transform.SetParent(popup, false);
+                UIBase pop = obj.GetComponent<UIBase>();
+                pop.Init(_data, callBackOnDone);
+                m_Popups.Add(popupName, pop);
+            }
+        }
+    }
+    public GameObject LoadPrefab(string objName, Transform parent=null)
+    {
+        GameObject obj = (GameObject)GameObject.Instantiate(Resources.Load(objName));
+        if (parent != null)
+            obj.transform.SetParent(parent, false);
+        return obj;
+    }
+
+    public bool RemoveListPopups(string popupName)
+    {
+        if (!m_Popups.ContainsKey(popupName))
+            return false;
+        m_Popups.Remove(popupName);
+
+        return true;
+    }
+}
